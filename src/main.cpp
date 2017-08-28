ddf4e9095f94d43c49a9684bccdfe43a7ff71d1e
Ubuntu <arunmozhi.rajavel@InfyLinux.12fjojqfooeero5nk1vuoylxid.ix.internal.cloudapp.net>
2017-08-28 17:18:07 +0000

    build will fail

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266WiFiMulti.h>
-#include <ESP8266HTTPClient.h>
+include <ESP8266HTTPClient.h>
 #include <ESP8266httpUpdate.h>
 #include <Ticker.h>
 
