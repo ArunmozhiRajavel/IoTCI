18ce50e91d9a8c31b2d2330532673eed85a7b40c
Ubuntu <arunmozhi.rajavel@InfyLinux.12fjojqfooeero5nk1vuoylxid.ix.internal.cloudapp.net>
2017-08-29 10:21:21 +0000

    dummy commit

--- git2aws.py
+++ git2aws.py
@@ -1,12 +1,16 @@
 from datetime import datetime, timedelta
+from github import Github
 
 def getLastCommitURL():
-    encrypted = Arun#moz3
-    g = Github('ArunmozhiRajavel', Arun#moz3)
-    org = g.get_organization('Self')
-    code = org.get_repo('IoTCI')
-    # limit to commits in past 24 hours
-    since = datetime.now() - timedelta(days=1)
-    commits = code.get_commits(since=since)
-    last = commits[0]
-    return last.html_url
+	g = Github('ArunmozhiRajavel', 'Arun#moz3')
+#	org = g.get_organization('ArunmozhiRajavelOrg')
+	code = g.get_user().get_repo('IoTCI')
+    	# limit to commits in past 24 hours
+	since = datetime.now() - timedelta(days=1)
+	commits = code.get_commits(since=since)
+	last = commits[0]
+	print(last.html_url)
+	raw_input("Press Enter to terminate.")
+	return last.html_url
+
+getLastCommitURL()
